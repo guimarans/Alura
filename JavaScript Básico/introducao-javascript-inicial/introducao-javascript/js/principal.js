65a43cdd2e5305d7e30ada8e012ae4c0f25c3a51
Kelly Guimarans <kygs@gft.com>
2019-01-31 09:50:46 -0200

    Aula 3

--- JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -1,5 +1,20 @@
 /*
-    Trocando o titulo
+  Trocando o titulo
 */
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
+
+/*
+  Informação do paciente
+*/
+var paciente = document.querySelector('#primeiro-paciente'); //Linha
+var peso     = paciente.querySelector('.info-peso').textContent; //Coluna
+var altura   = paciente.querySelector('.info-altura').textContent; //Coluna
+
+var imc = parseFloat(peso) / parseFloat(altura * altura);
+
+/* 
+  Alterando o resultado na tabela
+*/
+paciente.querySelector('.info-imc').textContent = imc;
+
