049af4ace2a1c711396c093f35ca3dd56349fdbf
Kelly Guimarans <kygs@gft.com>
2019-01-31 10:26:58 -0200

    Validação com operadores lógicos-JS Básico

--- JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -10,22 +10,29 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector('#primeiro-paciente'); //Linha
 var peso     = paciente.querySelector('.info-peso').textContent; //Coluna
 var altura   = paciente.querySelector('.info-altura').textContent; //Coluna
-
+var tdImc    = paciente.querySelector('.info-imc'); //Coluna imc
 /*
     Validação
 */
+var pesoValido = true; 
+var alturaValida = true;
 
-if(peso < 0 || peso > 750) {
-    console.log('Peso inválido');
-} else if (alura < 0.50 || altura > 3.10) {
-    console.log('Altura inválida');
+if(peso < 0 || peso >= 750) {
+    pesoValido = false;
+    /* texto de erro */
+    tdImc.textContent = 'Peso inválido';
+} else if (altura <= 0.50 || altura >= 3.10) {
+    alturaValida = false;
+    /* texto de erro */
+    tdImc.textContent = 'Altura inválida';
+}
+
+if( pesoValido && alturaValida) {
+    var imc = parseFloat(peso) / parseFloat(altura * altura);
+    /* adiciona o resultado na tabela */
+   tdImc.textContent = imc;
 }
 
 
-var imc = parseFloat(peso) / parseFloat(altura * altura);
 
-/* 
-  Alterando o resultado na tabela
-*/
-paciente.querySelector('.info-imc').textContent = imc;
 
