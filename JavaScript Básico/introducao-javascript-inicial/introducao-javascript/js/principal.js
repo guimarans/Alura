a5c111ce515ba87a77f8d3a44db10720e52a4081
Kelly Guimarans <kygs@gft.com>
2019-01-31 11:13:29 -0200

    Replicando a validação e o calculo para tds os clientes-JS Básico

--- JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/principal.js
+++ JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -7,32 +7,37 @@ titulo.textContent = "Aparecida Nutricionista";
 /*
   Informação do paciente
 */
-var paciente = document.querySelector('#primeiro-paciente'); //Linha
-var peso     = paciente.querySelector('.info-peso').textContent; //Coluna
-var altura   = paciente.querySelector('.info-altura').textContent; //Coluna
-var tdImc    = paciente.querySelector('.info-imc'); //Coluna imc
-/*
-    Validação
-*/
-var pesoValido = true; 
-var alturaValida = true;
+var pacientes = document.querySelectorAll('.paciente'); //Linha
 
-if(peso < 0 || peso >= 750) {
-    pesoValido = false;
-    /* texto de erro */
-    tdImc.textContent = 'Peso inválido';
-} else if (altura <= 0.50 || altura >= 3.10) {
-    alturaValida = false;
-    /* texto de erro */
-    tdImc.textContent = 'Altura inválida';
+for(var i = 0; i <= pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var peso     = paciente.querySelector('.info-peso').textContent; //Coluna
+    var altura   = paciente.querySelector('.info-altura').textContent; //Coluna
+    var tdImc    = paciente.querySelector('.info-imc'); //Coluna imc
+
+    /*
+        Validação
+    */
+    var pesoValido = true; 
+    var alturaValida = true;
+
+    if(peso < 0 || peso >= 750) {
+        pesoValido = false;
+        /* texto de erro */
+        tdImc.textContent = 'Peso inválido';
+    } else if (altura <= 0.50 || altura >= 3.10) {
+        alturaValida = false;
+        /* texto de erro */
+        tdImc.textContent = 'Altura inválida';
+    }
+
+    if( pesoValido && alturaValida) {
+        var imc = parseFloat(peso) / parseFloat(altura * altura);
+        /* adiciona o resultado na tabela */
+        tdImc.textContent = imc.toFixed(2);
+    }
+
 }
-
-if( pesoValido && alturaValida) {
-    var imc = parseFloat(peso) / parseFloat(altura * altura);
-    /* adiciona o resultado na tabela */
-   tdImc.textContent = imc;
-}
-
-
 
 
