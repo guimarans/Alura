dba459ed043e1461d0af82c0c16447d1a12cbf91
Kelly Guimarans <kygs@gft.com>
2019-02-05 08:59:15 -0200

    Filtro por evento de input

---
+++ JavaScript Básico/introducao-javascript-inicial/introducao-javascript/js/filtro.js
@@ -0,0 +1,17 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+    for (var i = 0; i < pacientes.length; i++) {
+        var paciente = pacientes[i];
+        var tdNome = paciente.querySelector(".info-nome");
+        var nome = tdNome.textContent;
+
+        if (nome != this.value) {
+            paciente.classList.add("invisivel");
+        } else {
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
