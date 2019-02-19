1e507e80ba206c1b999bb438173a710c948b00a1
Kelly Guimarans <kygs@gft.com>
2019-02-19 16:08:41 -0300

    Trabalhando com proxy I

--- JavaScript Avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript Avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,21 +3,24 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
-        let self = this;
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
-        this._listaNegociacoes = new ListaNegociacoes(
-            model => self._negociacoesView.update(model)
-        );
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         
-        this._mensagem = new Mensagem();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona', 'esvazia']
+        );
+         
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            this._mensagemView,
+            ['texto']
+        );       
     }
     
     adiciona(event) {
@@ -26,7 +29,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();   
     }
@@ -36,7 +38,6 @@ class NegociacaoController {
         this._listaNegociacoes.esvazia();
     
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
     
     _criaNegociacao() {
