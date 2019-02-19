1f4ac4cdc9b742749f24dec5a1d58c6442f794b9
Kelly Guimarans <kygs@gft.com>
2019-02-19 10:19:56 -0300

    melhorias da view

--- JavaScript Avancado/aluraframe/client/js/app/views/NegociacoesView.js
+++ JavaScript Avancado/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
-    constructor(elemento) {
+class NegociacoesView extends View {
 
-        this._elemento = elemento;
+    constructor(elemento) {
+        super(elemento);
     }
 
-    _template(model) {
+    template(model) {
 
         return `
         <table class="table table-hover table-bordered">
@@ -37,8 +37,4 @@ class NegociacoesView {
         `;
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
 }
---
+++ JavaScript Avancado/aluraframe/client/js/app/views/View.js
@@ -0,0 +1,13 @@
+class View {
+    constructor(elemento){
+        this._elemento = elemento;
+    }
+
+    template() {
+        throw new Error('O método templete deve ser implementado');
+    }
+
+    update(model) {
+        this._elemento.innerHTML = this._template(model);
+    }
+}
